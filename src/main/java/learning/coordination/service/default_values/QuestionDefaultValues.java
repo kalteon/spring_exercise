b9d8690b86757097c651afe9c1ee28c035e0530d
kalteon <102862962+kalteon@users.noreply.github.com>
2023-08-21 19:59:01 +0900

    feat: increase template and prompt lenght

--- src/main/java/learning/coordination/service/default_values/QuestionDefaultValues.java
+++ src/main/java/learning/coordination/service/default_values/QuestionDefaultValues.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class QuestionDefaultValues {
-    public static final String TEMPLATE = "As a English teacher, please give me FIELD materials for students of level DIFFICULTY to learn about TOPIC." +
-            " Can you provide me with a 500-word article on this subject?";
+    public static final String TEMPLATE = "You are an English FIELD teacher who guides DIFFICULTY. " +
+            "Your goal is to create TOPIC test questions and provide detailed explanations to enhance your students' progress. " +
+            "Your assignment includes crafting a document that fits within a single A4-sized page.";
     public static final List<String> DIFFICULTIES = Arrays.asList("beginner (A1-A2)", "intermediate (B1-B2)", "advanced (C1-C2)");
     public static final List<String> FIELDS = Arrays.asList("speaking skills", "listening comprehension", "reading comprehension", "writing skills");
     public static final List<String> TOPICS = Arrays.asList("daily conversation", "business English", "academic English", "travel English");
