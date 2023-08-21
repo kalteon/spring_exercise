41fe68c848d5bea31301f873086c0748a6567e31
kalteon <102862962+kalteon@users.noreply.github.com>
2023-08-21 20:06:53 +0900

    feat: ControllerExceptionHandler

---
+++ src/main/java/learning/coordination/exception/ControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package learning.coordination.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(e.getMessage()));
+    }
+
+    public static class ErrorResponse {
+        private String message;
+
+        public ErrorResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+}
