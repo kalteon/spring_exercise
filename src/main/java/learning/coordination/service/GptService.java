a01e36609ae1050190c41a12bc05c77fbfcb07aa
kalteon <102862962+kalteon@users.noreply.github.com>
2023-08-22 15:57:40 +0900

    feat: gpt api call and new template

--- src/main/java/learning/coordination/service/GptService.java
+++ src/main/java/learning/coordination/service/GptService.java
@@ -1,12 +1,19 @@
 package learning.coordination.service;
 
-import com.theokanning.openai.completion.CompletionRequest;
-import com.theokanning.openai.completion.CompletionResult;
+import com.theokanning.openai.completion.chat.ChatCompletionChoice;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.service.OpenAiService;
-import learning.coordination.service.api_keys.GptKey;
+import learning.coordination.service.api_keys.GptToken;
 import learning.coordination.service.default_values.GptDefaultValues;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,15 +24,36 @@ public class GptService {
 
     public void callGpt(Long id) {
         String prompt = questionService.findPromptById(id);
-        OpenAiService openAiService = new OpenAiService(GptKey.GPT_API_KEY);
-        CompletionRequest request = CompletionRequest.builder()
-                .prompt(prompt)
-                .maxTokens(GptDefaultValues.MAX_TOKENS)
-                .temperature(GptDefaultValues.TEMPERATURE)
+        String answer;
+        OpenAiService service = new OpenAiService(GptToken.GPT_API_TOKEN, Duration.ofSeconds(30));
+
+        final List<ChatMessage> messages = new ArrayList<>();
+        final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), prompt);
+        messages.add(systemMessage);
+
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
+                .builder()
                 .model(GptDefaultValues.MODEL)
+                .messages(messages)
+                .maxTokens(GptDefaultValues.MAX_TOKENS)
+                .n(1)
+                .logitBias(new HashMap<>())
                 .build();
-        CompletionResult response = openAiService.createCompletion(request);
-        String answer = response.getChoices().get(0).getText();
+
+        StringBuilder sentence = new StringBuilder();
+
+        service.streamChatCompletion(chatCompletionRequest)
+                .doOnError(Throwable::printStackTrace)
+                .blockingForEach(chatCompletionChunk -> {
+                    for (ChatCompletionChoice choice : chatCompletionChunk.getChoices()) {
+                        String content = choice.getMessage().getContent();
+                        sentence.append(content).append("");
+                    }
+                });
+
+        service.shutdownExecutor();
+
+        answer = sentence.toString();
         learningDataService.initLearningData(id);
         learningDataService.updateAnswer(id, answer);
     }
--- src/main/java/learning/coordination/service/default_values/QuestionDefaultValues.java
+++ src/main/java/learning/coordination/service/default_values/QuestionDefaultValues.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class QuestionDefaultValues {
     public static final String TEMPLATE = "You are an English FIELD teacher who guides DIFFICULTY. " +
-            "Your goal is to create TOPIC test questions and provide detailed explanations to enhance your students' progress. " +
-            "Your assignment includes crafting a document that fits within a single A4-sized page.";
-    public static final List<String> DIFFICULTIES = Arrays.asList("beginner (A1-A2)", "intermediate (B1-B2)", "advanced (C1-C2)");
+            "Your objective is to formulate fresh TOPIC test questions and provide detailed explanations to enhance your students' progress. " +
+            "You're required to create a document that stays within a 1000-character limit, with a minimum length of 200 characters.";
+    public static final List<String> DIFFICULTIES = Arrays.asList("beginner-level students (A1-A2)", "intermediate-level students (B1-B2)", "advanced-level students (C1-C2)");
     public static final List<String> FIELDS = Arrays.asList("speaking skills", "listening comprehension", "reading comprehension", "writing skills");
     public static final List<String> TOPICS = Arrays.asList("daily conversation", "business English", "academic English", "travel English");
     public static final String EMPTY_PROMPT = "empty prompt";
